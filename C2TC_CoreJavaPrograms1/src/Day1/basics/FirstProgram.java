f30ba788d903f93283eb6c6479f1c33671f80744
Sharanyamagesh <sharanyamagesh03@gmail.com>
2025-07-21 20:23:29 +0530

    Hello World!!!

---
+++ C2TC_CoreJavaPrograms1/src/Day1/basics/FirstProgram.java
@@ -0,0 +1,10 @@
+package Day1.basics;
+
+//Demonstration of simple java program
+
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
