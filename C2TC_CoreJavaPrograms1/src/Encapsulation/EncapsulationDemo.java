5e2779f19bd5f799573925ae564759a9e6d7721a
Sharanyamagesh <sharanyamagesh03@gmail.com>
2025-07-22 13:51:08 +0530

    Encapsulation and Oops concept

---
+++ C2TC_CoreJavaPrograms1/src/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,16 @@
+//Program to demonstrate class and object 
+package Encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(333);
+		obj.setName("Sharanya");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
