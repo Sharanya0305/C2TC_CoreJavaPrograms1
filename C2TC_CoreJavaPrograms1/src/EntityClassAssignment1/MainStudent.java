a10a3afdf856452bff76a28b962e92d9a19d47b9
Sharanyamagesh <sharanyamagesh03@gmail.com>
2025-07-22 12:20:49 +0530

    This is a program for Write a java program Student class and collect 3 students details in main program. 	(Package name: entityclassassignment1)

---
+++ C2TC_CoreJavaPrograms1/src/EntityClassAssignment1/MainStudent.java
@@ -0,0 +1,17 @@
+package EntityClassAssignment1;
+
+	public class MainStudent {
+	    public static void main(String[] args) {
+	        Student s1 = new Student(101, "Sharanya", "CSE");
+	        Student s2 = new Student(102, "Magesh", "IT");
+	        Student s3 = new Student(103, "Priya", "ECE");
+
+	        System.out.println("Student Details:");
+	        s1.display();
+	        s2.display();
+	        s3.display();
+	    }
+	}
+
+
+
